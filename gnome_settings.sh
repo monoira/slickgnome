6091a733b32a598435d5b2e45057d452a11fa29a
monoira <monoira@users.noreply.github.com>
2025-03-28 02:11:13 +0400

    feat: disabled screen lock

--- gnome_settings.sh
+++ gnome_settings.sh
@@ -29,8 +29,8 @@ gsettings set org.gnome.shell.extensions.ding show-home false
 # turns off mouse acceleration
 gsettings set org.gnome.desktop.peripherals.mouse accel-profile 'flat'
 
-# change screen locking after inactivity from 5 minutes to 15 minutes ( 15 minutes is 900 seconds )
-gsettings set org.gnome.desktop.session idle-delay 900
+# disable "screen locking after period of inactivity"
+gsettings set org.gnome.desktop.session idle-delay 0
 
 # NOTE: ubuntu specific settings
 # disable update notifications
